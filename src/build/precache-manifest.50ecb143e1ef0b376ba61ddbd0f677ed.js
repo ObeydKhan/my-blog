1e6a8ddaca17eaae9313e689f2bf68ad5c36f766
Khan, Obeyd <Obeyd.Khan@asurion.com>
2020-06-07 23:35:04 -0400

    First Commit

---
+++ src/build/precache-manifest.50ecb143e1ef0b376ba61ddbd0f677ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9da43592fa789eec16b2a569acd029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e7b571141602eafa74",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6b24c0248e4cc554c66b",
+    "url": "/static/js/2.99650adb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99650adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e7b571141602eafa74",
+    "url": "/static/js/main.055bf0cf.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
